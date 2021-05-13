22a8660fd539f3c112aebcafb508af944666f63b
Sankalp <sankalpofficial16@gmail.com>
2021-05-13 13:30:13 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
-export default function App() {
+const JustifyContentBasics = () => {
+  const [justifyContent, setJustifyContent] = useState("space-evenly");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PreviewLayout
+      label="Sankalp Chake"
+      selectedValue={justifyContent}
+      values={[
+        
+      ]}
+      setSelectedValue={setJustifyContent}
+    >
+      <View
+        style={[styles.box, { backgroundColor: "#7fff00" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "#8b0000" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "steelblue" }]}
+      />
+    </PreviewLayout>
   );
-}
+};
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, { [label]: selectedValue }]}>
+      {children}
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection:'row',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 8,
+    backgroundColor: "aliceblue",
+  },
+  box: {
+    width: 80,
+    height: 80,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    marginTop:10,
+    fontSize: 24,
   },
 });
+
+export default JustifyContentBasics;
